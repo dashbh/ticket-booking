2cd45bee7929e7f787bd36dbf7044227eef9d253
CGU <cgu@CGUs-MacBook-Air.local>
2023-09-17 20:39:05 +0530

    Created K8 Deployment & Services

--- api-gateway/src/main.ts
+++ api-gateway/src/main.ts
@@ -1,10 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { ProxyModule } from './proxy.module';
-const promClient = require('prom-client');
 
 async function bootstrap() {
   const app = await NestFactory.create(ProxyModule, { bodyParser: false });
-  promClient.collectDefaultMetrics();
   await app.listen(9000);
 }
 bootstrap();
